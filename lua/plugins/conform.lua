774e9cbf102e83af139d165cb76cc6edb90a48f1
Nick Tzaperas <nick@nijotz.com>
2025-06-30 15:27:30 -0700

    eslint_d for linting plugin

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,14 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      javascript = { "eslint_d" },
+      typescript = { "eslint_d" },
+      javascriptreact = { "eslint_d" },
+      typescriptreact = { "eslint_d" },
+      json = { "prettier" },
+      markdown = { "prettier" },
+      lua = { "stylua" },
+    },
+  },
+}
