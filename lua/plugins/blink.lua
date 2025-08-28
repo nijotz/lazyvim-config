7c3ac5e05bd5153de5230ba7afbc752d0e0980d9
Nick Tzaperas <nick@nijotz.com>
2025-08-28 14:15:24 -0700

    Use C-y for selecting autocomplete

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,14 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    completion = {
+      ghost_text = {
+        enabled = false,
+      },
+    },
+    keymap = {
+      preset = "default",
+      ["<C-y>"] = { "select_and_accept" },
+    },
+  },
+}
