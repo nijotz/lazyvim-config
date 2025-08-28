8964e52784adb7ff59fbfc5ef052391e01f88052
Nick Tzaperas <nick@nijotz.com>
2025-08-28 14:15:55 -0700

    Turn off noice

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,8 +1,19 @@
 return {
   "folke/noice.nvim",
+  enabled = false,
   opts = {
     cmdline = {
       view = "cmdline",
+    },
+    routes = {
+      {
+        filter = {
+          event = "msg_show",
+          kind = "",
+          find = "=",
+        },
+        view = "cmdline", -- send them to the command line instead of floating popup
+      },
     },
   },
 }
