a84e662bb01b20c4b5a005b5764eb7c29fb27ade
Nick Tzaperas <nick@nijotz.com>
2025-11-22 16:16:14 -0800

    Fix pyright "hints"

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -2,5 +2,21 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     inlay_hints = { enabled = false },
+    servers = {
+      -- Turn off unwanted "hints" from pyright
+      -- https://github.com/microsoft/pyright/discussions/5852#discussioncomment-6874502
+      pyright = {
+        capabilities = {
+          textDocument = {
+            publishDiagnostics = {
+              tagSupport = {
+                valueSet = { 2 },
+              },
+            },
+          },
+        },
+      },
+      ruff_lsp = {},
+    },
   },
 }
