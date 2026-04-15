b6a88c07df09f759ac2110a36f69f173cc8d99e0
Nick Tzaperas <nick@nijotz.com>
2026-04-15 22:21:11 +0000

    Turn off esc-j/k moving lines around

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Turn off esc-j/k moving lines around
+vim.keymap.del({ "n", "i", "v" }, "<A-j>")
+vim.keymap.del({ "n", "i", "v" }, "<A-k>")
